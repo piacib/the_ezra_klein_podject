ac7940b1e2b21147d7816a174d2ef848cd085de3
piacib <ben.piacitelli@gmail.com>
2023-04-13 20:34:34 -0700

    create component structure

---
+++ my-klein-fe/src/components/Table/Table.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container } from './Table.styles';
+
+const Table = () => {
+  return (
+    <Container>
+      <h1>Table</h1>
+    </Container>
+  );
+};
+
+export default Table;
