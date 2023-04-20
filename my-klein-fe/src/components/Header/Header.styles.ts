9d56ddf288131db195fec170d2065d9dc0a28232
piacib <ben.piacitelli@gmail.com>
2023-04-19 22:26:41 -0700

    moved h1 styles to seperate component

--- my-klein-fe/src/components/Header/Header.styles.ts
+++ my-klein-fe/src/components/Header/Header.styles.ts
@@ -8,28 +8,23 @@ export const Container = styled.div`
   display: flex;
   align-items: flex-end;
   justify-content: space-between;
-  h1 {
-    font-size: 2rem;
-    margin: 0;
-    text-align: end;
-    padding-right: ${(props) => props.theme.spacing.xs};
-    color: ${(props) => props.theme.colors.accent};
-  }
+
   border-bottom: 1rem solid ${(props) => props.theme.colors.accent};
-  @media ${devices.mobileL} {
-    h1 {
-      font-size: 2.5rem;
-    }
-  }
-  @media ${devices.tablet} {
-    h1 {
-      font-size: 3rem;
-    }
-  }
-  @media ${devices.laptop} {
-    h1 {
-      font-size: 4rem;
-    }
-  }
 `;
 
+export const Title = styled.h1`
+  font-size: 2rem;
+  margin: 0;
+  text-align: end;
+  padding-right: ${(props) => props.theme.spacing.xs};
+  color: ${(props) => props.theme.colors.accent};
+  @media ${devices.mobileL} {
+    font-size: 2.5rem;
+  }
+  @media ${devices.tablet} {
+    font-size: 3rem;
+  }
+  @media ${devices.laptop} {
+    font-size: 4rem;
+  }
+`;
---
+++ my-klein-fe/src/components/Header/Header.tsx
@@ -0,0 +1,21 @@
+import { Container, Title } from "./Header.styles";
+import Books from "../BookStack/BookStack";
+import Paths from "../../router/paths";
+import { Nav, StyledLink } from "./Nav.styles";
+
+const Header = () => {
+  return (
+    <>
+      <Nav>
+        <StyledLink to={Paths.home}>Home</StyledLink>
+        <StyledLink to={Paths.randombook}>Random Book</StyledLink>
+      </Nav>
+      <Container>
+        <Books />
+        <Title>The Ezra Klein Podject</Title>
+      </Container>
+    </>
+  );
+};
+
+export default Header;
