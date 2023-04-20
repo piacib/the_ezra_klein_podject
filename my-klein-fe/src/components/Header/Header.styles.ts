dda4e3e73ecd24916aebc0021de40cac9d8e6ff7
piacib <ben.piacitelli@gmail.com>
2023-04-19 22:22:06 -0700

    added header styles

--- my-klein-fe/src/components/Header/Header.styles.ts
+++ my-klein-fe/src/components/Header/Header.styles.ts
@@ -1,5 +1,35 @@
-import styled from 'styled-components';
+import styled from "styled-components";
+import { devices } from "../../styles/devices";
 
 export const Container = styled.div`
-
+  background-color: ${(props) => props.theme.colors.background};
+  padding: ${(props) => props.theme.spacing.xs} 0;
+  padding-bottom: 0;
+  display: flex;
+  align-items: flex-end;
+  justify-content: space-between;
+  h1 {
+    font-size: 2rem;
+    margin: 0;
+    text-align: end;
+    padding-right: ${(props) => props.theme.spacing.xs};
+    color: ${(props) => props.theme.colors.accent};
+  }
+  border-bottom: 1rem solid ${(props) => props.theme.colors.accent};
+  @media ${devices.mobileL} {
+    h1 {
+      font-size: 2.5rem;
+    }
+  }
+  @media ${devices.tablet} {
+    h1 {
+      font-size: 3rem;
+    }
+  }
+  @media ${devices.laptop} {
+    h1 {
+      font-size: 4rem;
+    }
+  }
 `;
+
