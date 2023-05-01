8b3bf80bd1d8857ae38645d2f5fcab9d3bd5708f
piacib <ben.piacitelli@gmail.com>
2023-05-01 15:52:25 -0700

    added sizing and set colors to rgb

--- frontend/src/styles/theme.ts
+++ frontend/src/styles/theme.ts
@@ -8,9 +8,12 @@ export const theme: DefaultTheme = {
     xl: "7rem",
   },
   colors: {
-    ezraYellow: "#e6a153",
-    accent: "#0f3d4f",
-    background: "#e2ded0",
-    tableRowSecondary: "aliceblue",
+    ezraYellow: "rgb(230, 161, 83)",
+    accent: "rgb(15, 61, 79)",
+    background: "rgb(226, 222, 208)",
+    tableRowSecondary: "rgb(240 248 255)",
   },
+  sizing: {
+    footerHeight:'5rem'
+  }
 };
