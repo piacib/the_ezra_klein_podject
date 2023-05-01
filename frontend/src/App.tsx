3a019a234ac09c5f95b79a5aa72200ddeee8d3d2
piacib <ben.piacitelli@gmail.com>
2023-05-01 15:40:36 -0700

    added footer

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,14 +2,15 @@ import { useState } from "react";
 import { Outlet } from "react-router-dom";
 import ErrorBoundWithSuspense from "./components/ErrorBoundary/ErrorBoundWithSuspense";
 import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
     <ErrorBoundWithSuspense>
       <div className="App">
         <Header />
         <Outlet />
+        <Footer/>
       </div>
     </ErrorBoundWithSuspense>
   );
