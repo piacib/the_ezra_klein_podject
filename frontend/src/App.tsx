e5788a475afdadc21f2c136a151364f759eb1891
piacib <ben.piacitelli@gmail.com>
2023-05-01 15:53:01 -0700

    removed useState

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import { Outlet } from "react-router-dom";
 import ErrorBoundWithSuspense from "./components/ErrorBoundary/ErrorBoundWithSuspense";
 import Header from "./components/Header/Header";
