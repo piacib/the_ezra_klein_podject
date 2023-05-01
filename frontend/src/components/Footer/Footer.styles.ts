e89ef6ea666d4599ec2702c51ea9cdf83da79e6c
piacib <ben.piacitelli@gmail.com>
2023-05-01 15:52:50 -0700

    set up footer components

--- frontend/src/components/Footer/Footer.styles.ts
+++ frontend/src/components/Footer/Footer.styles.ts
@@ -1,1 +1,15 @@
 import styled from "styled-components";
+
+export const FooterContainer = styled.div`
+  min-height:${props=>props.theme.sizing.footerHeight};
+  margin-top:${props=>props.theme.spacing.m};
+  width:100%;
+  background-color: ${props=> props.theme.colors.accent};
+`;
+export const SpotifyLink = styled.a`
+  
+`;
+export const GithubLink = styled.a`
+  
+`;
+
