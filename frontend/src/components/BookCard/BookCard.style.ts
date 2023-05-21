bb166b9c1de41b11b91b9cdf911210f87f488c1e
piacib <ben.piacitelli@gmail.com>
2023-05-21 11:01:07 -0700

    added card style

---
+++ frontend/src/components/BookCard/BookCard.style.ts
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+export const BookCardContainer = styled.div`
+  margin: 1rem;
+  height: 18rem;
+`;
+const EllipsisOverflow = styled.p`
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  width: 15ch;
+  :hover {
+    white-space: normal;
+  }
+`;
+export const BookTitle = styled(EllipsisOverflow)`
+  margin: 0 auto;
+  padding-bottom: 0;
+`;
+export const BookAuthor = styled(EllipsisOverflow)`
+  margin-top: 0;
+`;
