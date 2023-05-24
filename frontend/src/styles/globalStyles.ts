1ef9abfea3b55edfa71eb5fda82a5e3477eb059f
piacib <ben.piacitelli@gmail.com>
2023-05-24 09:13:22 -0700

    added font sizes and colors

--- frontend/src/styles/globalStyles.ts
+++ frontend/src/styles/globalStyles.ts
@@ -1,5 +1,6 @@
 // globalStyles.js
 import { createGlobalStyle } from "styled-components";
+import { devices } from "./devices";
 
 const GlobalStyle = createGlobalStyle`
   body {
@@ -11,8 +12,32 @@ const GlobalStyle = createGlobalStyle`
   #root, body {
     background-color: ${(props) => props.theme.colors.background};
   }
-  p,a {
+  p,a, h1,h2,h3,h4 {
     color: ${(props) => props.theme.colors.accent}
+  }
+  h1 {
+    font-size: 2rem;
+  @media ${devices.mobileL} {
+    font-size: 2.5rem;
+  }
+  @media ${devices.tablet} {
+    font-size: 3rem;
+  }
+  @media ${devices.laptop} {
+    font-size: 4rem;
+  }
+  }
+  h2 {
+    font-size: 1rem;
+  @media ${devices.mobileL} {
+    font-size: 1.5rem;
+  }
+  @media ${devices.tablet} {
+    font-size: 2rem;
+  }
+  @media ${devices.laptop} {
+    font-size: 3rem;
+  }
   }
 `;
 
