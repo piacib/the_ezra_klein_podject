081e214e778c18fec9a4d380979d80c58c8ef856
piacib <ben.piacitelli@gmail.com>
2023-05-24 09:23:49 -0700

    added page for route

---
+++ frontend/src/pages/BookPage/BookPage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import BookDisplay from "../../components/BookDisplay/BookDisplay";
+
+const BookPage = () => {
+  return (
+    <>
+      <BookDisplay />
+    </>
+  );
+};
+
+export default BookPage;
