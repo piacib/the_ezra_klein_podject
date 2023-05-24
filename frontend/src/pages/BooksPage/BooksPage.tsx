596f5f4faecf4ca0fb05377dae66d30947d4cd9e
piacib <ben.piacitelli@gmail.com>
2023-05-24 09:14:37 -0700

    changed books display name

--- frontend/src/pages/BooksPage/BooksPage.tsx
+++ frontend/src/pages/BooksPage/BooksPage.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import BooksDisplay from "../../components/BooksDisplay/BooksDisplay";
+import BooksDisplay from "../../components/BooksCategoriesDisplay/BooksCategoriesDisplay";
 const View = styled.div`
   display: flex;
   flex-direction: column;
