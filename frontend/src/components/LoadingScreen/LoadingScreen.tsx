3269734fbe70fe6c5a8ad580e2d4029a23c6c84e
piacib <ben.piacitelli@gmail.com>
2023-06-02 12:10:07 -0700

    added load screen and svg

--- frontend/src/components/LoadingScreen/LoadingScreen.tsx
+++ frontend/src/components/LoadingScreen/LoadingScreen.tsx
@@ -1,9 +1,33 @@
-import React from 'react'
-
-const LoadingScreen = () => {
+import React from "react";
+import LoadingSVG from "./loading.svg";
+import styled, { keyframes } from "styled-components";
+const spinning = keyframes`
+    from {
+        transform: rotate(359deg);
+    }
+    to {
+        transform: rotate(0deg);
+    }
+  
+  `;
+const LoadingIcon = styled.img`
+  animation: ${spinning} 4s infinite linear;
+  height: 9rem;
+  width: 9rem;
+  margin: auto;
+`;
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  display: grid;
+  place-items: center;
+`;
+export const LoadingScreen = () => {
   return (
-    <div>LoadingScreen</div>
-  )
-}
+    <Container>
+      <LoadingIcon src={LoadingSVG} alt="Loading" />;
+    </Container>
+  );
+};
 
-export default LoadingScreen
+export default LoadingScreen;
