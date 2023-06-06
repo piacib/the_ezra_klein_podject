e73c2950a1dd0288be5157abde489759911e7cf1
piacib <ben.piacitelli@gmail.com>
2023-06-06 10:09:37 -0700

    added page to display all categories

---
+++ frontend/src/pages/AllCategoriesPage/AllCategoriesPage.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+import AllCategoriesDisplay from "../../components/AllCategoriesDisplay/AllCategoriesDisplay";
+import { Page } from "../Page.styles";
+
+const AllCategoriesPage = () => {
+  return (
+    <Page>
+      <AllCategoriesDisplay />
+    </Page>
+  );
+};
+
+export default AllCategoriesPage;
