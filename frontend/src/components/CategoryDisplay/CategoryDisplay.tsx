feaa2addfcd91d64595b65b091f929ae0c7e63ec
piacib <ben.piacitelli@gmail.com>
2023-06-06 15:35:30 -0700

    fixed category filter

--- frontend/src/components/CategoryDisplay/CategoryDisplay.tsx
+++ frontend/src/components/CategoryDisplay/CategoryDisplay.tsx
@@ -4,31 +4,29 @@ import { alldata } from "../../alldata";
 import BookCard from "../BookCard/BookCard";
 import { BooksContainer, CategoryHeader } from "./CategoryDisplay.styles";
 import { Title } from "../Header/Header.styles";
+
 const booksByCategory = (category: string | undefined) => {
   const list: string[] = [];
   if (!category) {
     return list;
   }
-  console.log(category);
-  Object.entries(alldata).forEach(([title, obj]) => {
-    if (obj.categories) {
-      obj.categories.every((x) => {
-        if (x.includes(category)) {
-          if (obj.id) {
-            list.push(obj.id);
-            return false;
-          }
-          return true;
-        }
-      });
+  const entries = Object.values(alldata);
+  entries.forEach((x) => {
+    if (x.categories) {
+      const results = x.categories.filter((cat) =>
+        cat.includes(category)
+      ).length;
+      if (results && x.id) {
+        list.push(x.id);
+      }
     }
   });
+
   return list;
 };
 const CategoryDisplay = () => {
   const { id } = useParams();
   const books = booksByCategory(id);
-  console.log(books);
   return (
     <>
       <CategoryHeader>{id}</CategoryHeader>
@@ -39,12 +37,6 @@ const CategoryDisplay = () => {
       </BooksContainer>
     </>
   );
-
-  // return (
-  //   <div>
-  //     <h1>No books in {id} found</h1>
-  //   </div>
-  // );
 };
 
 export default CategoryDisplay;
