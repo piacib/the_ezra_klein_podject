37e1889159b481ce971572ea9ba05b5de7035d20
piacib <ben.piacitelli@gmail.com>
2023-06-06 09:58:18 -0700

    added styling

---
+++ frontend/src/components/CategoryDisplay/CategoryDisplay.styles.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+import { Flex } from "../Carousel/Carousel.style";
+export const BooksContainer = styled(Flex)`
+  flex-wrap: wrap;
+  * {
+    width: 10rem;
+  }
+`;
+export const CategoryHeader = styled.h2`
+`
