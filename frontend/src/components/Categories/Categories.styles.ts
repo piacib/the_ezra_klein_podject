046f91a03708044fa9cd073f7a73fca07257e56a
piacib <ben.piacitelli@gmail.com>
2023-06-06 14:58:08 -0700

    added css

--- frontend/src/components/Categories/Categories.styles.ts
+++ frontend/src/components/Categories/Categories.styles.ts
@@ -57,6 +57,13 @@ export const CategoriesContainer = styled.div`
   display: flex;
   flex-wrap: wrap;
 `;
+const comicBackground = css`
+  border: 0.4rem solid ${(props) => props.theme.colors.accent};
+  box-shadow: -8px 7px 0px 0px ${(props) => props.theme.colors.accent};
+  background: url('data:image/svg+xml;utf8,<svg width="100" height="100" transform="rotate(25)" opacity="0.8" version="1.1" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg"><g  fill="%23d68810"><circle cx="25" cy="25" r="12.5"/><circle cx="75" cy="75" r="12.5"/><circle cx="75" cy="25" r="12.5"/><circle cx="25" cy="75" r="12.5"/></g></svg>'),
+    ${(props) => props.theme.colors.ezraYellow};
+  background-size: 13px, 100%;
+`;
 export const bubbleCSS = css`
   max-width: 500px;
   margin: 2rem;
@@ -66,13 +73,9 @@ export const bubbleCSS = css`
   font-size: 1.5rem;
   line-height: 1.5rem;
   padding: 0.5rem 1.25rem;
-  box-shadow: -8px 7px 0px 0px ${(props) => props.theme.colors.accent};
   letter-spacing: 0.2rem;
-  border: 0.4rem solid ${(props) => props.theme.colors.accent};
 
-  background: url('data:image/svg+xml;utf8,<svg width="100" height="100" transform="rotate(25)" opacity="0.8" version="1.1" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg"><g  fill="%23d68810"><circle cx="25" cy="25" r="12.5"/><circle cx="75" cy="75" r="12.5"/><circle cx="75" cy="25" r="12.5"/><circle cx="25" cy="75" r="12.5"/></g></svg>'),
-    ${(props) => props.theme.colors.ezraYellow};
-  background-size: 13px, 100%;
+  ${comicBackground}
   font-weight: 700;
 `;
 export const BubbleLink = styled(Link)`
