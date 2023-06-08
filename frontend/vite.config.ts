7efb487279b8c369863adda236e7dd107fc59f3a
piacib <ben.piacitelli@gmail.com>
2023-06-08 09:12:08 -0700

    added base path

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
-
+export const baseURl = "/the_ezra_klein_podject/";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: baseURl,
   test: {
     globals: true,
     environment: "jsdom",
