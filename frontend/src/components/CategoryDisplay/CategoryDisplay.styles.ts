c9efa8503860b7028412aaa460d8e9d32d0025a2
piacib <ben.piacitelli@gmail.com>
2023-06-08 08:42:47 -0700

    centered content

--- frontend/src/components/CategoryDisplay/CategoryDisplay.styles.ts
+++ frontend/src/components/CategoryDisplay/CategoryDisplay.styles.ts
@@ -2,9 +2,9 @@ import styled from "styled-components";
 import { Flex } from "../Carousel/Carousel.style";
 export const BooksContainer = styled(Flex)`
   flex-wrap: wrap;
+  justify-content: center;
   * {
     width: 10rem;
   }
 `;
-export const CategoryHeader = styled.h2`
-`
+export const CategoryHeader = styled.h2``;
