1c6844b5978deb0bedc8511519c8383a67145335
piacib <ben.piacitelli@gmail.com>
2023-06-15 09:22:32 -0700

    removed unused imports

--- frontend/src/router/router.tsx
+++ frontend/src/router/router.tsx
@@ -1,13 +1,8 @@
-import {
-  createBrowserRouter,
-  createHashRouter,
-  RouterProvider,
-} from "react-router-dom";
+import { createHashRouter } from "react-router-dom";
 import App from "../App";
 import TableView from "../pages/TableView";
-import RandomBookView from "../pages/RandomBookView";
 import ErrorPage from "../pages/ErrorPage/ErrorPage";
-import paths, { pathGenerator } from "./paths";
+import paths from "./paths";
 import BooksPage from "../pages/BooksPage/BooksPage";
 import CategoryPage from "../pages/CategoryPage/CategoryPage";
 import BookPage from "../pages/BookPage/BookPage";
