595174450d9f26cf89d2efa3a4c8099f9ff1cf3d
piacib <ben.piacitelli@gmail.com>
2023-06-15 09:21:27 -0700

    set random pook path to rndpath generator

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,6 +1,6 @@
 import { Container, Title } from "./Header.styles";
 import Books from "../BookStack/BookStack";
-import paths from "../../router/paths";
+import paths, { pathGenerator } from "../../router/paths";
 import { Nav, StyledLink } from "./Nav.styles";
 
 const Header = () => {
@@ -8,7 +8,7 @@ const Header = () => {
     <>
       <Nav>
         <StyledLink to={paths.home}>Home</StyledLink>
-        <StyledLink to={paths.randombook}>Random Book</StyledLink>
+        <StyledLink to={pathGenerator.randombook()}>Random Book</StyledLink>
         <StyledLink to={paths.books}>Books</StyledLink>
         <StyledLink to={paths.allcategories}>Categories</StyledLink>
         <StyledLink to={paths.table}>Table</StyledLink>
