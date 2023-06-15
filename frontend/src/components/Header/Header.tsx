12b80a87f0807c46042ee07375cc428af3c32ab4
piacib <ben.piacitelli@gmail.com>
2023-06-15 10:00:02 -0700

    added random book generator to link

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -2,13 +2,20 @@ import { Container, Title } from "./Header.styles";
 import Books from "../BookStack/BookStack";
 import paths, { pathGenerator } from "../../router/paths";
 import { Nav, StyledLink } from "./Nav.styles";
+import { useReducer, useRef, useState } from "react";
 
 const Header = () => {
+  const [randomBook, updateBook] = useReducer(
+    () => pathGenerator.randombook(),
+    pathGenerator.randombook()
+  );
   return (
     <>
       <Nav>
         <StyledLink to={paths.home}>Home</StyledLink>
-        <StyledLink to={pathGenerator.randombook()}>Random Book</StyledLink>
+        <StyledLink onClick={updateBook} to={randomBook}>
+          Random Book
+        </StyledLink>
         <StyledLink to={paths.books}>Books</StyledLink>
         <StyledLink to={paths.allcategories}>Categories</StyledLink>
         <StyledLink to={paths.table}>Table</StyledLink>
